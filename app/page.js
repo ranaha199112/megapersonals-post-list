e43c79083a1eccdc11e8a546629f63721dee5c84
Zayn Yusuf <yusue786@gmail.com>
2024-06-05 20:32:38 +0600

    adding route

--- app/page.js
+++ app/page.js
@@ -1,26 +1,7 @@
-"use client";
-import { useState } from "react";
-import LoginForm from "./components/LoginForm";
-
-export default function Home({ params }) {
-  const [homepage, setHomepage] = useState(false);
-  const { adminId, posterId } = params;
-
+export default function Home() {
   return (
-    <>
-      {!homepage ? (
-        <div className="">
-          <img src="/images/main.jpeg" />
-          <button
-            className="w-3/4 flex  flex-col justify-center items-center mx-auto text-[#ffff] mt -3 px-3 py-2 rounded bg-[#00aaff]"
-            onClick={() => setHomepage(true)}
-          >
-            Post list Check
-          </button>
-        </div>
-      ) : (
-        <LoginForm adminId={adminId} posterId={posterId} />
-      )}
-    </>
+    <div>
+      <p>Megapersonals Post List</p>
+    </div>
   );
 }
